028b1bc070304bba6bbe782bda84978f6fbea2f9
leuel-a <leuela1993@gmail.com>
2024-04-11 13:13:28 +0300

    fix: removed all the all domain entity classes

--- src/ft.transaction-management/ft.transaction-management.Domain/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-using ft.transaction_management.Domain.Common;
-
-namespace ft.transaction_management.Domain.Entities;
-
-public class Category : BaseDomainEntity
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
