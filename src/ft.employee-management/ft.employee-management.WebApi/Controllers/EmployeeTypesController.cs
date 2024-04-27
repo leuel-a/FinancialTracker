e9f8959cfcd98c509064e08853d6662a56419f9a
leuel-a <leuela1993@gmail.com>
2024-04-27 23:31:31 +0300

    add the CreatedEmployeeType action

--- src/ft.employee-management/ft.employee-management.WebApi/Controllers/EmployeeTypesController.cs
+++ src/ft.employee-management/ft.employee-management.WebApi/Controllers/EmployeeTypesController.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Threading.Tasks;
+using ft.employee_management.Application.Dtos.EmployeeTypeDto;
+using ft.employee_management.Application.Features.EmployeeType.Requests.Commands;
+using ft.employee_management.Application.Features.EmployeeType.Requests.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ft.employee_management.WebApi.Controllers;
@@ -6,8 +12,32 @@ namespace ft.employee_management.WebApi.Controllers;
 [ApiController]
 public class EmployeeTypesController : ControllerBase
 {
-    public EmployeeTypesController()
+    private readonly IMediator _mediator;
+
+    public EmployeeTypesController(IMediator mediator)
     {
-        
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllEmployeeTypes()
+    {
+        var employeeTypes = await _mediator.Send(new GetAllEmployeeTypesRequest());
+        return Ok(employeeTypes);
+    }
+
+    [HttpGet("{id:int}", Name = "GetEmployeeTypeById")]
+    public async Task<IActionResult> GetEmployeeTypeById(int id)
+    {
+        var employeeType = await _mediator.Send(new GetEmployeeTypeRequest() { Id = id });
+        return Ok(employeeType);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateEmployeeType([FromBody] CreateEmployeeTypeDto employeeTypeDto)
+    {
+        var employeeType = await _mediator.Send(new CreateEmployeeTypeCommand()
+            { CreateEmployeeTypeDto = employeeTypeDto });
+        return CreatedAtRoute("GetEmployeeTypeById", new { Id = employeeType.Id }, employeeType);
     }
 }
