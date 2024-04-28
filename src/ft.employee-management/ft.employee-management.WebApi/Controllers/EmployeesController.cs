4538c2e7539b4f77ced697e705ed933782fea71b
leuel-a <leuela1993@gmail.com>
2024-04-28 14:52:32 +0300

    add the Delete and Update handlers and action controlers

--- src/ft.employee-management/ft.employee-management.WebApi/Controllers/EmployeesController.cs
+++ src/ft.employee-management/ft.employee-management.WebApi/Controllers/EmployeesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using ft.employee_management.Application.Dtos.EmployeeDto;
 using ft.employee_management.Application.Features.Employee.Requests.Queries;
 using ft.employee_management.Application.Features.Employee.Requests.Commands;
+using ft.employee_management.Domain.Entities;
 
 namespace ft.employee_management.WebApi.Controllers;
 
@@ -37,5 +38,19 @@ public class EmployeesController : ControllerBase
     {
         var result = await _mediator.Send(new CreateEmployeeCommand { EmployeeDto = employee });
         return CreatedAtRoute("GetEmployeeById", new { id = result.Id }, result);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteEmployee(int id)
+    {
+        await _mediator.Send(new DeleteEmployeeCommand() { Id = id });
+        return NoContent();
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateEmployee(int id, [FromBody] EmployeeDto employee)
+    {
+        var result = await _mediator.Send(new UpdateEmployeeCommand { EmployeeDto = employee });
+        return Ok(result);
     }
 }
